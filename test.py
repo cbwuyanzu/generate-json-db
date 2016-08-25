25ab10e6744e71a219940863285f5800ac2cfcd1
cbwuyanzu <277850402@qq.com>
2016-08-26 05:47:07 +0800

    data

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
+# coding: utf-8
 import json
 
-json_file = '1.json'
-f = open(json_file)
-json_str = f.read()
-result = json.loads(json_str)
-data_list = result[u'values']
-print len(data_list)
-count = 0
-for x in data_list:
-    print (x[0] - 1467304200000) / 1800000, "+++", count
-    count += 1
+for i in range(1, 9):
+    json_file = str(i) + '.json'
+    f = open(json_file)
+    json_str = f.read()
+    result = json.loads(json_str)
+    f.close()
+    data_list = result[u'values']
+    count = 0
+    data_list_new = []
+    for x in data_list:
+        # print (x[0] - 1467304200000) / 1800000, "+++", count
+        x = x[0:-1]
+        if count == 0:
+            print x
+        count += 1
+        data_list_new.append(x)
+    result[u'values'] = data_list_new
+    json_file = 'light' + str(i) + '.json'
+    f = open(json_file, "w")
+    f.write(json.dumps(result))
